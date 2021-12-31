2f140291d1a89bf840c6d9325210df40f40534a2
Tanner Davies <tanner.e.davies@gmail.com>
2021-12-31 14:30:58 -0700

    Test auth middleware

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,20 +1,18 @@
-use actix_web::{error, FromRequest};
+pub(crate) use actix_web::dev::Payload;
+pub(crate) use actix_web::{error, FromRequest, HttpRequest};
 use futures::future;
 
-use crate::utils::jwt;
+pub(crate) use crate::utils::jwt;
 
 #[derive(Debug)]
 pub struct AuthorizedUserClaims(pub jwt::TokenClaims);
 
 impl FromRequest for AuthorizedUserClaims {
-    type Error = actix_web::Error;
+    type Error = error::Error;
     type Future = future::Ready<Result<Self, Self::Error>>;
     type Config = ();
 
-    fn from_request(
-        req: &actix_web::HttpRequest,
-        _payload: &mut actix_web::dev::Payload,
-    ) -> Self::Future {
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
         const INVALID_TOKEN_MSG: &'static str = "Token is invalid";
 
         let auth_header = match req.headers().get("Authorization") {
@@ -48,5 +46,130 @@ impl FromRequest for AuthorizedUserClaims {
         };
 
         future::ok(AuthorizedUserClaims(claims))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use actix_web::http::header;
+    use actix_web::test;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_jwt_user_auth_middleware() {
+        let user_id = Uuid::new_v4();
+
+        let token = jwt::generate_access_token(&user_id).unwrap();
+
+        let req = test::TestRequest::with_header(
+            "authorization",
+            format!("Bearer {}", &token.to_string()),
+        )
+        .to_http_request();
+
+        let user_claims = AuthorizedUserClaims::from_request(&req, &mut Payload::None)
+            .into_inner()
+            .unwrap()
+            .0;
+
+        assert_eq!(&user_claims.uid, &user_id);
+
+        let req = test::TestRequest::with_header(
+            "Authorization",
+            format!("bearer {}", &token.to_string()),
+        )
+        .to_http_request();
+
+        let user_claims = AuthorizedUserClaims::from_request(&req, &mut Payload::None)
+            .into_inner()
+            .unwrap()
+            .0;
+
+        assert_eq!(&user_claims.uid, &user_id);
+    }
+
+    #[test]
+    fn test_auth_middleware_rejects_request_without_auth_header() {
+        let user_id = Uuid::new_v4();
+
+        let token = jwt::generate_access_token(&user_id).unwrap();
+
+        let req = test::TestRequest::with_header(
+            header::CONTENT_TYPE,
+            format!("bearer {}", &token.to_string()),
+        )
+        .to_http_request();
+
+        let res = AuthorizedUserClaims::from_request(&req, &mut Payload::None).into_inner();
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_auth_middleware_rejects_header_without_bearer_keyword() {
+        let user_id = Uuid::new_v4();
+
+        let token = jwt::generate_access_token(&user_id).unwrap();
+
+        let req = test::TestRequest::with_header(
+            header::AUTHORIZATION,
+            format!("{}", &token.to_string()),
+        )
+        .to_http_request();
+
+        let res = AuthorizedUserClaims::from_request(&req, &mut Payload::None).into_inner();
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_auth_middleware_rejects_header_without_token() {
+        let user_id = Uuid::new_v4();
+
+        let _ = jwt::generate_access_token(&user_id).unwrap();
+
+        let req = test::TestRequest::with_header(header::AUTHORIZATION, "bearer").to_http_request();
+        let res = AuthorizedUserClaims::from_request(&req, &mut Payload::None).into_inner();
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_auth_middleware_rejects_invalid_token() {
+        let user_id = Uuid::new_v4();
+
+        let token = jwt::generate_access_token(&user_id).unwrap().to_string();
+
+        // Remove the last char of the token
+        let broken_token = &token[0..token.len() - 1];
+
+        let req = test::TestRequest::with_header(
+            header::AUTHORIZATION,
+            format!("bearer {}", broken_token),
+        )
+        .to_http_request();
+
+        let res = AuthorizedUserClaims::from_request(&req, &mut Payload::None).into_inner();
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_auth_middleware_rejects_refresh_token_in_auth_header() {
+        let user_id = Uuid::new_v4();
+
+        let token = jwt::generate_refresh_token(&user_id).unwrap();
+
+        let req = test::TestRequest::with_header(
+            header::AUTHORIZATION,
+            format!("bearer {}", &token.to_string()),
+        )
+        .to_http_request();
+
+        let res = AuthorizedUserClaims::from_request(&req, &mut Payload::None).into_inner();
+
+        assert!(res.is_err());
     }
 }
